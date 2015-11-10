33b476de62dddba7b46ed896daba4a2b4a4bdc4c
Lehner <ericj@buffalo.edu>
2015-11-10 09:29:21 -0500

    Fixed source data criteria helper class.

--- lib/hqmf-parser/2.0/source_data_criteria_helper.rb
+++ lib/hqmf-parser/2.0/source_data_criteria_helper.rb
@@ -1,18 +1,18 @@
 module HQMF2
+  # Generates the Source Data Criteria from the entries in the HQMF
   class SourceDataCriteriaHelper
-  	def self.identifier(criteria)
-
-      sha256 = ""
+    def self.identifier(criteria)
+      sha256 = ''
       sha256 << "#{criteria.code_list_id}:"
       sha256 << "#{criteria.definition}:"
-      sha256 << (criteria.status.nil? ? "<nil>:" : "#{criteria.status}:")
-      sha256 << (criteria.specific_occurrence.nil? ? "<nil>:" : "#{criteria.specific_occurrence}:")
-      sha256 << (criteria.specific_occurrence.nil? ? "<nil>:" : "#{criteria.specific_occurrence_const}:")
+      sha256 << "#{criteria.status}:"
+      sha256 << "#{criteria.specific_occurrence}:"
+      sha256 << "#{criteria.specific_occurrence_const}:"
       sha256 << "#{criteria.variable}:"
-      sha256 << (criteria.children_criteria.nil? ? "<nil>:" : "#{criteria.children_criteria.sort.join(',')}:")
+      sha256 << (criteria.children_criteria.nil? ? '<nil>:' : "#{criteria.children_criteria.sort.join(',')}:")
 
       Digest::SHA256.hexdigest sha256
-  	end
+    end
 
     def self.should_reject(dc)
       dc.definition == 'derived'
@@ -42,19 +42,20 @@ module HQMF2
 
     def self.get_source_data_criteria_list(full_criteria_list, data_criteria_references = {}, occurrences_map = {})
       # currently, this will erase the sources if the ids are the same, but will not correct references later on
-      source_data_criteria = full_criteria_list.map{|entry| SourceDataCriteriaHelper.as_source_data_criteria(entry, data_criteria_references, occurrences_map)}
+      source_data_criteria = full_criteria_list.map do |entry|
+        SourceDataCriteriaHelper.as_source_data_criteria(entry, data_criteria_references, occurrences_map)
+      end
       collapsed_source_data_criteria_map = {}
       uniq_source_data_criteria = {}
       source_data_criteria.each do |sdc|
         identifier = SourceDataCriteriaHelper.identifier(sdc)
-        if uniq_source_data_criteria.has_key? identifier
+        if uniq_source_data_criteria.key? identifier
           collapsed_source_data_criteria_map[sdc.id] = uniq_source_data_criteria[identifier].id
         else
           uniq_source_data_criteria[identifier] = sdc
         end
       end
-      return uniq_source_data_criteria.values.reject {|dc| SourceDataCriteriaHelper.should_reject(dc)}, collapsed_source_data_criteria_map
+      [uniq_source_data_criteria.values.reject { |dc| SourceDataCriteriaHelper.should_reject(dc) }, collapsed_source_data_criteria_map]
     end
-
   end
 end
